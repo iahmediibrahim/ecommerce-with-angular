dc057465a50f28caefaec6e10fa0cd57b4f70194
swethusiast <aitc.dev@gmail.com>
2021-04-08 23:23:22 +0200

    latest change

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
-
+app.use(
+    cors({
+        origin: '*',
+        methods: [ 'GET', 'POST', 'PATCH', 'DELETE', 'PUT' ],
+        allowedHeaders: 'Content-Type, Authorization, origin, X-Requested-With, Accept',
+    }),
+);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
