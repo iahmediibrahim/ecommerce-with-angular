e2ba79cb464d289dd5b118d89f445c1377f947ed
swethusiast <aitc.dev@gmail.com>
2021-05-05 00:25:14 +0200

    latest updates

--- app.js
+++ app.js
@@ -3,12 +3,37 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const swaggerUI = require('swagger-ui-express');
+// const swaggerJSDoc = require('swagger-jsdoc');
+const options = {
+    swaggerOptions: {
+        url: 'http://petstore.swagger.io/v2/swagger.json',
+    },
+};
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'Shop API',
+            description: 'Backend Api',
+            contact: {
+                name: 'Ahmed Ibrahim',
+            },
+            servers: 'http://localhost:3636',
+        },
+    },
+    apis: [ 'app.js', '.routes/*.js' ],
+};
+
+// const swaggerDocs = swaggerJSDoc(swaggerOptions);
 
 // import routes
 const productsRouter = require('./routes/products');
 const ordersRouter = require('./routes/orders');
-
+const authRouter = require('./routes/auth');
+const usersRouter = require('./routes/users');
 const app = express();
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(null, options));
 
 app.use(
     cors({
@@ -22,9 +47,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+/**
+ * @swagger
+ * /api/products:
+ *   get:
+ *    description: Get All Products
+ *
+ */
 // use routers
 app.use('/api/products', productsRouter);
 app.use('/api/orders', ordersRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/users', usersRouter);
 
 module.exports = app;
--- routes/products.js
+++ routes/products.js
@@ -111,4 +111,28 @@ router.get('/category/:categoryName', (req, res) => {
         })
         .catch((err) => console.log(err));
 });
+
+router.delete('/delete/:prodId', (req, res) => {
+    let prodId = req.params.prodId;
+
+    if (!isNaN(prodId)) {
+        database
+            .table('products')
+            .filter({ id: prodId })
+            .remove()
+            .then((successNum) => {
+                if (successNum == 1) {
+                    res.status(200).json({
+                        message: `Record deleted with product id ${prodId}`,
+                        status: 'success',
+                    });
+                } else {
+                    res.status(500).json({ status: 'failure', message: 'Cannot delete the product' });
+                }
+            })
+            .catch((err) => res.status(500).json(err));
+    } else {
+        res.status(500).json({ message: 'ID is not a valid number', status: 'failure' });
+    }
+});
 module.exports = router;
